c8d935f5b1a3638f8e893db7446d5b0c11ab0bb9
BlucaBBZW <Luca_Bisanti@sluz.ch>
2023-06-19 10:03:13 +0200

    Dokumentation

--- src/aufgabe4.py
+++ src/aufgabe4.py
@@ -4,8 +4,11 @@ import psutil
 import matplotlib.pyplot as plt
 import threading
 
+
+# Verbindung zur Datenbank herstellen
 connectionString = "mongodb+srv://lucibluc:luci69@cluster0.rtex13l.mongodb.net/?retryWrites=true&w=majority"
 db = Database(connectionString)
+# Eine Sammlung ("Collection") in der Datenbank abrufen
 col = db.get_collection("Power", "check")
 
 class Power:
@@ -19,12 +22,15 @@ class Power:
             self.timestamp = timestamp
 
     def save_to_database(self):
+        # Speichert die Power-Daten in der Datenbank.
+        # Löscht älteste Datensätze, falls die Anzahl der Dokumente in der Datenbank größer als 10000 ist.
         col.insert_one(self.__dict__)
         if col.count_documents({}) > 10000:
             oldest_logs = col.find().sort('timestamp', 1).limit(col.count_documents({}) - 10000)
             col.delete_many({'_id': {'$in': [log['_id'] for log in oldest_logs]}})
 
     def plt(self):
+        # Zeigt ein Diagramm mit den CPU- und RAM-Daten der letzten 10000 Datensätze an.
         logs = col.find().sort('timestamp', 1).limit(10000)
         cpu_values = []
         ram_values = []
@@ -44,21 +50,27 @@ class Power:
         plt.show()
 
 def power_stats():
+    # Ruft kontinuierlich die CPU- und RAM-Daten ab, speichert sie in der Datenbank und gibt sie aus.
+    # Zeigt auch das Diagramm der Daten an.
     while True:
+        # CPU- und RAM-Daten abrufen
         cpu_percent = psutil.cpu_percent(interval=1)
         mem = psutil.virtual_memory()
         ram_total = mem.total
         ram_used = mem.used
 
+        # Power-Objekt erstellen und in der Datenbank speichern
         power = Power(cpu_percent, ram_total, ram_used)
         power.save_to_database()
 
+        # Daten ausgeben
         print("CPU Usage: {}%".format(power.cpu))
         print("RAM Total: {} MB".format(power.ram_total))
         print("RAM Used: {} MB".format(power.ram_used))
         print("Timestamp: {}".format(power.timestamp))
         print("---------------------------------")
 
+        # Daten mit der App ausgeben
         power = Power(None, None, None)
         power.plt()
 
